e65f9e8eef8442c5513446a2d376373a1cc498ef
Naran Babha <naranbabha@NaransComputer.local>
2017-08-31 11:17:10 -0400

    Intial Setup of Page. Tabs created and some styling done

---
+++ main.js
@@ -0,0 +1,12 @@
+function close1() {
+    $('#projectsContent').collapse("hide");
+    $('#other').collapse("hide");
+}
+function close2() {
+    $('#aboutContent').collapse("hide");
+    $('#other').collapse("hide");
+}
+function close3() {
+    $('#projectsContent').collapse("hide");
+    $('#aboutContent').collapse("hide");
+}
